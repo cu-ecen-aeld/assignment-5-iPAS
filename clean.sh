9a0ef902eab3fdb348f894154d14cb2e7a6edf22
iPAS <ptiwatthanont@gmail.com>
2022-07-03 11:32:23 +0700

    Add clean.sh

---
+++ clean.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Script to build buildroot configuration
+#Author: Siddhant Jajoo
+
+source shared.sh
+
+#EXTERNAL_REL_BUILDROOT=../base_external
+#git submodule init
+#git submodule sync
+#git submodule update
+
+set -e 
+cd `dirname $0`
+
+echo 'Clean Evaporatedly'
+make -C buildroot distclean BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT}
+
